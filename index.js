fb1ab930ef654bf8f3aeeed7e1e17de92a2876b3
unknown <soulmamoudou0@gmail.com>
2025-10-27 10:19:11 +0000

    Modification pour le deployement

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://back-carnet-adress.onrender.com',
   credentials: true
 }));
 
